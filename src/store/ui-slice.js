6bc374c7d71b438d8d596e080f10b37ecc70dfc5
andreigugy <andrei.gugy@gmail.com>
2026-01-28 13:49:41 +0200

    add Notification component and logic to display it

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,12 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const intialState = { cartIsVisible: false };
+const intialState = { cartIsVisible: false, notification: null };
 const uiSlice = createSlice({
 	name: "ui",
 	initialState: intialState,
 	reducers: {
 		toggle(state) {
 			state.cartIsVisible = !state.cartIsVisible;
+		},
+		showNotification(state, action) {
+			state.notification = {
+				status: action.payload.status,
+				title: action.payload.title,
+				message: action.payload.message,
+			}; //status is pendion, error or success
 		},
 	},
 });
